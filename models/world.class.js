f7bbf12bcd0e532d64144f852b5b5ee614523a00
DavidV0 <davidvelickovic@hotmail.com>
2023-12-29 22:00:37 +0100

    bigger world
    more background and cleaner world.js

--- models/world.class.js
+++ models/world.class.js
@@ -1,32 +1,13 @@
 class World {
   character = new Character();
-  enemies = [new Chicken(), new Chicken(), new Chicken(), new Chicken()];
+  enemies = level1.enemies;
+  backgroundObject = level1.backgroundObjects;
+  clouds = level1.clouds;
   ctx;
   canvas;
   keyboard;
-  clouds = [new Cloud(), new Cloud(), new Cloud()];
   camera_x = 0;
-  backgroundObject = [
-    new BackgroundObject("./img/5_background/layers/air.png", 0, 80),
 
-    new BackgroundObject(
-      "./img/5_background/layers/3_third_layer/1.png",
-      0,
-      80
-    ),
-
-    new BackgroundObject(
-      "./img/5_background/layers/2_second_layer/1.png",
-      0,
-      80
-    ),
-
-    new BackgroundObject(
-      "./img/5_background/layers/1_first_layer/1.png",
-      0,
-      80
-    ),
-  ];
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
     this.canvas = canvas;
@@ -41,13 +22,12 @@ class World {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	this.ctx.translate(this.camera_x, 0)
+    this.ctx.translate(this.camera_x, 0);
     this.addObjectsToMap(this.backgroundObject);
     this.addObjectsToMap(this.clouds);
     this.addObjectsToMap(this.enemies);
     this.addToMap(this.character);
-	this.ctx.translate(-this.camera_x, 0)
-
+    this.ctx.translate(-this.camera_x, 0);
 
     //Draw() wird immer wieder aufgerufen
     let self = this;
@@ -68,15 +48,15 @@ class World {
         this.ctx.save();
         this.ctx.translate(mo.width, 0);
         this.ctx.scale(-1, 1);
-		mo.x = mo.x* -1
+        mo.x = mo.x * -1;
       }
       this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
 
-	  if(mo.otherDirection){
-		mo.x = mo.x* -1
+      if (mo.otherDirection) {
+        mo.x = mo.x * -1;
 
-		this.ctx.restore();
-	  }
+        this.ctx.restore();
+      }
     } catch (e) {
       console.warn("Error loading Image");
       console.log("Could not load image,", this.img.src);
