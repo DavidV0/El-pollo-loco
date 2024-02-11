56e8bcdeca2381d526a98e1fb6e647ebea993c17
DavidV0 <davidvelickovic@hotmail.com>
2024-02-11 19:18:05 +0530

    style and description of moving

--- models/world.class.js
+++ models/world.class.js
@@ -1,139 +1,139 @@
 class World {
-  character = new Character();
-  startGameBtn = document.getElementById("startGameBtn");
-  level = level1;
-  enemies = level1.enemies;
-  coins = level1.coins;
-  backgroundObject = level1.backgroundObject;
-  clouds = level1.clouds;
-  ctx;
-  canvas;
-  keyboard;
-  camera_x = 0;
-  statusBar = new StatusBar();
-  bottleStatusBar = new BottleStatusBar();
-  coinStatusBar = new CoinStatusBar();
-  titleScreen = new TitleScreen(720, 480);
-  throwableObjects = [];
+	character = new Character();
+	startGameBtn = document.getElementById('startGameBtn');
+	level = level1;
+	enemies = level1.enemies;
+	coins = level1.coins;
+	backgroundObject = level1.backgroundObject;
+	clouds = level1.clouds;
+	ctx;
+	canvas;
+	keyboard;
+	camera_x = 0;
+	statusBar = new StatusBar();
+	bottleStatusBar = new BottleStatusBar();
+	coinStatusBar = new CoinStatusBar();
+	titleScreen = new TitleScreen(720, 480);
+	throwableObjects = [];
 
-  constructor(canvas, keyboard) {
-    this.ctx = canvas.getContext("2d");
-    this.canvas = canvas;
-    this.keyboard = keyboard;
-    this.setWorld();
-    this.draw();
-    this.startGameBtn.addEventListener("click", () => this.startGame());
-  }
+	constructor(canvas, keyboard) {
+		this.ctx = canvas.getContext('2d');
+		this.canvas = canvas;
+		this.keyboard = keyboard;
+		this.setWorld();
+		this.draw();
+		this.startGameBtn.addEventListener('click', () => this.startGame());
+	}
 
-  startGame() {
-    this.character.otherDirection = false;
-    setTimeout(() => {
-      this.character.speed = 5.5;
-      this.startGameBtn.classList.add("d-none");
-      this.titleScreen = null;
+	startGame() {
+		this.character.otherDirection = false;
+		setTimeout(() => {
+			this.character.speed = 5.5;
+			this.cleanMenuScreen();
+			this.run();
+		}, 200);
+	}
 
-      this.run();
-    }, 200);
-  }
+	cleanMenuScreen() {
+		this.startGameBtn.classList.add('d-none');
+		this.titleScreen = null;
+	}
 
-  run() {
-    setInterval(() => {
-      this.checkCollisions();
-      this.checkThrowObjects();
-    }, 200);
-  }
+	run() {
+		setInterval(() => {
+			this.checkCollisions();
+			this.checkThrowObjects();
+		}, 200);
+	}
 
-  checkThrowObjects() {
-    if (this.keyboard.D) {
-      let bottle = new ThrowableObject(
-        this.character.x + 50,
-        this.character.y + 50
-      );
-      this.throwableObjects.push(bottle);
-    }
-  }
+	checkThrowObjects() {
+		if (this.keyboard.D) {
+			let bottle = new ThrowableObject(
+				this.character.x + 50,
+				this.character.y + 50
+			);
+			this.throwableObjects.push(bottle);
+		}
+	}
 
-  checkCollisions() {
-    this.level.enemies.forEach((enemy) => {
-      if (this.character.isColliding(enemy)) {
-        this.character.hit();
-        this.statusBar.setPercentage(this.character.energy);
-      }
-    });
-  }
+	checkCollisions() {
+		this.level.enemies.forEach((enemy) => {
+			if (this.character.isColliding(enemy)) {
+				this.character.hit();
+				this.statusBar.setPercentage(this.character.energy);
+			}
+		});
+	}
 
-  setWorld() {
-    this.character.world = this;
-  }
+	setWorld() {
+		this.character.world = this;
+	}
 
-  draw() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.translate(this.camera_x, 0);
-    this.addObjectsToMap(this.level.backgroundObject);
-    this.addObjectsToMap(this.throwableObjects);
-    
-    this.addObjectsToMap(this.level.clouds);
-    this.addObjectsToMap(this.level.enemies);
-    this.addToMap(this.character);
-    // --------Space for fixed objects
+	draw() {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.ctx.translate(this.camera_x, 0);
+		this.addObjectsToMap(this.level.backgroundObject);
+		this.addObjectsToMap(this.throwableObjects);
 
-    this.ctx.translate(-this.camera_x, 0); // Back
-    this.addToMap(this.statusBar);
-    this.addToMap(this.bottleStatusBar);
-    this.addToMap(this.coinStatusBar);
-    
-    this.ctx.translate(this.camera_x, 0); // Forward
-    this.ctx.translate(-this.camera_x, 0);
-    this.addToMap(this.titleScreen);
+		this.addObjectsToMap(this.level.clouds);
+		this.addObjectsToMap(this.level.enemies);
+		this.addToMap(this.character);
+		// --------Space for fixed objects
 
-    //Draw() wird immer wieder aufgerufen
-    let self = this;
-    requestAnimationFrame(function () {
-      self.draw();
-    });
-  }
+		this.ctx.translate(-this.camera_x, 0); // Back
+		this.addToMap(this.statusBar);
+		this.addToMap(this.bottleStatusBar);
+		this.addToMap(this.coinStatusBar);
 
-  addObjectsToMap(objects) {
-  
-      try {
-        objects.forEach((obj) => {
-        this.addToMap(obj);
-        });
-      } catch (error) {
-        console.log(obj)
-      }
-  
-  
-  }
+		this.ctx.translate(this.camera_x, 0); // Forward
+		this.ctx.translate(-this.camera_x, 0);
+		this.addToMap(this.titleScreen);
 
-  addToMap(mo) {
-    try {
-      if (mo.otherDirection) {
-        this.flipImage(mo);
-      }
-      mo.draw(this.ctx);
+		//Draw() wird immer wieder aufgerufen
+		let self = this;
+		requestAnimationFrame(function () {
+			self.draw();
+		});
+	}
 
-      mo.drawFrame(this.ctx);
+	addObjectsToMap(objects) {
+		try {
+			objects.forEach((obj) => {
+				this.addToMap(obj);
+			});
+		} catch (error) {
+			console.log(obj);
+		}
+	}
 
-      if (mo.otherDirection) {
-        this.flipImageBack(mo);
-      }
-    } catch (e) {
-      console.warn("Error loading Image");
-      console.log("Could not load image,", this.img, this.img.src);
-    }
-  }
+	addToMap(mo) {
+		try {
+			if (mo.otherDirection) {
+				this.flipImage(mo);
+			}
+			mo.draw(this.ctx);
 
-  flipImage(mo) {
-    this.ctx.save();
-    this.ctx.translate(mo.width, 0);
-    this.ctx.scale(-1, 1);
-    mo.x = mo.x * -1;
-  }
+			mo.drawFrame(this.ctx);
 
-  flipImageBack(mo) {
-    mo.x = mo.x * -1;
+			if (mo.otherDirection) {
+				this.flipImageBack(mo);
+			}
+		} catch (e) {
+			console.warn('Error loading Image');
+			console.log('Could not load image,', this.img, this.img.src);
+		}
+	}
 
-    this.ctx.restore();
-  }
+	flipImage(mo) {
+		this.ctx.save();
+		this.ctx.translate(mo.width, 0);
+		this.ctx.scale(-1, 1);
+		mo.x = mo.x * -1;
+	}
+
+	flipImageBack(mo) {
+		mo.x = mo.x * -1;
+
+		this.ctx.restore();
+	}
 }
