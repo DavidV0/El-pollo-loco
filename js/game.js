5f0089b64ac03c3f901582675b896ec9da4a919a
DavidV0 <davidvelickovic@hotmail.com>
2024-02-13 19:16:39 +0530

    .

--- js/game.js
+++ js/game.js
@@ -45,46 +45,6 @@ function toggleMusic() {
 	}
 }
 
-/**
- * Changes the info view to the first page.
- */
-function changeToFirstPage() {
-	let firstPage = document.getElementById('first-page');
-	let secondPage = document.getElementById('second-page');
-	secondPage.innerHTML = '';
-	firstPage.classList.remove('d-none');
-}
-
-/**
- * Changes the info view to the second page.
- */
-function changeToSecondPage() {
-	let firstPage = document.getElementById('first-page');
-	let secondPage = document.getElementById('second-page');
-	firstPage.classList.add('d-none');
-	secondPage.innerHTML = /*html*/ `<p class="second-headline">Instructions</p><div class="story-text">Pepe can jump on the head of 
-        the chickens and the chicks to 
-        kill them. He can also kill them with the collected bottles. The final boss can be defeated with 
-        several hits of the salsa bottles.</div><div class="back-box">
-        <img src="./img/extern_imgs/back_arrow.png" alt="next-page-image"class="next-page-image" onclick="changeToFirstPage()">
-        <div class="next-text">Back</div></div><div class="next-box"><div class="next-text">Next</div>
-        <img src="./img/extern_imgs/forward_arrow.png" alt="next-page-image" class="next-page-image" onclick="changeToThirdPage()"></div>`;
-}
-
-/**
- * Changes the info view to the third page.
- */
-function changeToThirdPage() {
-	let secondPage = document.getElementById('second-page');
-	secondPage.innerHTML = /*html*/ `<p class="second-headline">The Tales of the Chickens Rebellion</p><div class="story-text">
-    The tranquil harmony between humans and chickens shattered when an enigmatic mutation infiltrated the nearby flocks, 
-    rendering them defiant and formidable adversaries. Spearheaded by a colossal, 
-    mutated rooster, the insurgent poultry wreaked havoc across the region. 
-    Pepe stands as humanity's final beacon of hope....<br>
-        <div class="back-box"><img src="./img/extern_imgs/back_arrow.png" 
-        alt="next-page-image"class="next-page-image" onclick="changeToSecondPage()"><div class="next-text">Back</div></div>`;
-}
-
 document.addEventListener('keydown', (e) => {
 	if (e.keyCode == 39) {
 		keyboard.RIGHT = true;
